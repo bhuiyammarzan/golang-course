49fdb1648273a7b5f806c443be976d8329ef6fbc
Bhuiyan Marzan <samimbuian12@gmail.com>
2025-11-21 06:51:16 +0000

    variables

---
+++ 3_variables/variables.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main (){
+	// var name string = "golang"
+
+	// infer
+	// var name = "golang"
+	// fmt.Println(name)
+
+	// bool
+	// var isAdult = true
+	// fmt.Println(isAdult)
+
+	// number
+	// var age int = 30
+	// fmt.Println(age)
+
+	// shorthand syntex
+	// name := "golang"
+	// fmt.Println(name)
+
+	// without assign value
+	var name string
+	name= "golang"
+	fmt.Println(name)
+
+	// var price float32 = 50.0
+	// var price = 50.5
+	price := 50.5
+	fmt.Println(price)
+}
